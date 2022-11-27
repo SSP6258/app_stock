2d450e1882d7308622e2df0a420ad1fb26f18311
SSP6258 <ssp6258@yahoo.com.tw>
2022-11-27 17:56:42 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -274,11 +274,12 @@ def fn_st_chart_bar(df):
     if 'order' not in st.session_state.keys():
         st.session_state['order'] = '績效(%)'
 
-    dft_idx = st.session_state['kpi'].index(st.session_state['order']) if st.session_state['order'] in st.session_state['kpi'] else 0
-    st.session_state['order'] = cs[2].selectbox(f'選擇排序:', options=st.session_state['kpi'], index=dft_idx)
+    if len(st.session_state['kpi']) > 0:
+        dft_idx = st.session_state['kpi'].index(st.session_state['order']) if st.session_state['order'] in st.session_state['kpi'] else 0
+        st.session_state['order'] = cs[2].selectbox(f'選擇排序:', options=st.session_state['kpi'], index=dft_idx)
 
-    df_sids.sort_values(by=[st.session_state['order']], inplace=True, ascending=False, ignore_index=True)
-    df_sids.reset_index(inplace=True)
+        df_sids.sort_values(by=[st.session_state['order']], inplace=True, ascending=False, ignore_index=True)
+        df_sids.reset_index(inplace=True)
 
     def fn_add_digit(x):
         for i in range(3 - len(str(x))):
