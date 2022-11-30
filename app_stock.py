fba73c1ed5d6106be8682514d298a338a23aa75e
SSP6258 <ssp6258@yahoo.com.tw>
2022-11-30 20:10:38 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -303,8 +303,7 @@ def fn_st_chart_bar(df):
         st.session_state['order_typ'] = cs[1].selectbox(f'排序方向:', options=['大 --> 小', '小 --> 大'], index=0)
         st.session_state['order'] = cs[1].selectbox(f'排序指標:', options=st.session_state['kpi'], index=0)
 
-        st.pyplot(df_sids.hist(by=st.session_state['order']))
-
+        st.pyplot(df_sids.hist(column=st.session_state['order']))
 
         ascending = st.session_state['order_typ'] == '小 --> 大'
         df_sids.sort_values(by=[st.session_state['order']], inplace=True, ascending=ascending, ignore_index=True)
