39acd7dd6d4fea5175d92896efcd8dedac129b64
SSP6258 <ssp6258@yahoo.com.tw>
2022-11-30 20:48:48 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -306,7 +306,8 @@ def fn_st_chart_bar(df):
         st.session_state['order'] = cs[1].selectbox(f'排序指標:', options=st.session_state['kpi'], index=0)
 
         fig = px.histogram(df_sids, x=st.session_state['order'])
-        cs[2].plotly_chart(fig, use_container_width=False, height=10)
+        fig.update_layout(height=50)
+        cs[2].plotly_chart(fig, use_container_width=False)
 
         ascending = st.session_state['order_typ'] == '小 --> 大'
         df_sids.sort_values(by=[st.session_state['order']], inplace=True, ascending=ascending, ignore_index=True)
