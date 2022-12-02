142ff362ea2a3d9a17dd82e479eaf1db57508d91
SSP6258 <ssp6258@yahoo.com.tw>
2022-12-02 18:54:51 +0800

    init version

---
+++ app_utils.py
@@ -0,0 +1,21 @@
+import plotly.graph_objs as go
+import plotly.express as px
+
+
+def fn_gen_plotly_bar(df, x_col, y_col,
+                      txt_col=None, color_col=None, v_h='h', margin=None,
+                      title=None, height=None, width=None, op=None, barmode=None,
+                      legend=True, lg_title=None, lg_pos=None, lg_x=None):
+
+    fig = px.bar(df, x=x_col, y=y_col, orientation=v_h, title=title, text=txt_col, color=color_col,
+                 width=width, height=height, opacity=op)
+
+    fig.update_layout(margin=margin,
+                      showlegend=legend,
+                      legend=dict(
+                          title=lg_title,
+                          orientation=lg_pos,
+                          x=lg_x)
+                      )
+
+    return fig
