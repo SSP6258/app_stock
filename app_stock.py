6f3e7b1498344994a70fefc10eb3637162fcbab6
SSP6258 <ssp6258@yahoo.com.tw>
2022-12-04 16:02:53 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -174,9 +174,9 @@ def fn_st_stock_sel(df_all):
 
     fn_st_add_space(1)
     c1, c2 = st.columns([2.5, 1])
-
+    sels = c1.columns(3)
     with c1.form(key='sel'):
-        sels = c1.columns(3)
+
         dic_cfg["sel_rat"] = sels[0].slider('勝率門檻(%)', min_value=40, max_value=100, value=dic_cfg["sel_rat"])
         dic_cfg["sel_corr"] = sels[1].slider('相關性門檻', min_value=0.5, max_value=1.0, value=dic_cfg["sel_corr"])
         dic_cfg["sel_price"] = sels[2].slider('股價範圍', min_value=0, max_value=1000, value=(0, dic_cfg["sel_price"]))
