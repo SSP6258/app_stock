f51c933bf107ca6ac9350931a80375f19aa11907
SSP6258 <ssp6258@yahoo.com.tw>
2022-12-04 15:35:32 +0800

    init version

--- app_stock_fb.py
+++ app_stock_fb.py
@@ -12,7 +12,7 @@ dic_cfg = {
     'batch_size': 5,
     'sel_rat': 50,
     'sel_price': 500,
-    'sel_corr': 0.8,
+    'sel_corr': 0.75,
     'sel_rat_h': 90,
     'stocks': ['1535'],
     'stock_file': 'stock.csv',
