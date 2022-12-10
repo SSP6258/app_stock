be7a11c12dbffdbb2e0ae2a5501217fe08c09688
SSP6258 <ssp6258@yahoo.com.tw>
2022-12-10 09:24:12 +0800

    init version

--- app_utils.py
+++ app_utils.py
@@ -23,7 +23,7 @@ def fn_gen_plotly_bar(df, x_col, y_col,
                           # family="Courier New, monospace",
                           size=14,
                           ),
-                      legend_x='r',
+                      legend_x='right',
                       legend=dict(
                           title=lg_title,
                           # orientation=lg_pos,
