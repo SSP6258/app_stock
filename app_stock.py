7125b3e05c539e50eed8a50a7924e2351f1a8828
SSP6258 <ssp6258@yahoo.com.tw>
2023-03-15 22:45:58 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -1068,7 +1068,7 @@ def fn_show_hist_price(df, df_mops, key='hist_price'):
         # sid_name = st.selectbox('觀察個股:', options=df['sid_name'], index=0, key=key)
         # df_sid = df[df["sid_name"] == sid_name]
 
-        sid = st.text_input('個股代碼:', value=df['代碼'].values[0])
+        sid = st.text_input('股票代碼:', value=df['代碼'].values[0])
 
         fn_st_add_space(2)
         st.form_submit_button('選擇')
@@ -1080,6 +1080,11 @@ def fn_show_hist_price(df, df_mops, key='hist_price'):
 
     df_all = dic_df['stock_all']
     df_sid = df_all[df_all['sid']==sid]
+
+    if df_sid.shape[0] == 0:
+        st.error(f'Sorry 您輸入的股票代碼 {sid} 不在資料庫喔 ~')
+        assert False, f'Sorry 您輸入的股票代碼 {sid} 不在資料庫喔 ~'
+
     sid_name = df_sid['sid_name'].values[0]
 
     url_WantRich = rf'{dic_url["WantRich"]}{sid}'
