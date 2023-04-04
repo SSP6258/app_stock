4c6a4cc496876e12a3afdfbe78df742b4b13a604
SSP6258 <ssp6258@yahoo.com.tw>
2023-04-04 18:21:05 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -629,6 +629,10 @@ def fn_st_stock_sel(df_all):
                             '相關性_殖利率': '殖利率<br>相關性'}
 
         df_show.rename(columns=show_cols_rename, inplace=True)
+        latest_only = True
+        if latest_only:
+            df_show = df_show[df_show['大戶比']!= '']
+
         fn_st_add_space(1)
         st.write(df_show.to_html(escape=False, index=True), unsafe_allow_html=True)
 
