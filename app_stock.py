7acfb5d046aed6e72af2defe55042cfdfdda5da9
SSP6258 <ssp6258@yahoo.com.tw>
2023-04-13 21:13:03 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -521,7 +521,7 @@ def fn_st_stock_sel(df_all):
                 days = delta_time.days
 
                 profs.append(prof + 0.000001 * i)
-                sign = '' if prof < 10 else ' 🚀'
+                sign = '' if prof < 20 else ' 🚀'
                 metrics.append([f'⭐{sid_name} {sid}', f'{price_new}{sign}', f'{prof}% / {days}天'])
 
                 # cs[j].metric(f'{sid_name} {sid}', f'{price_new}', f'{prof}% / {days}天', delta_color='inverse')
