a937c564f0d0b3435552a719ebe71f6cb53110de
SSP6258 <ssp6258@yahoo.com.tw>
2023-04-22 09:42:44 +0800

    init version

--- app_stock.py
+++ app_stock.py
@@ -426,7 +426,7 @@ def fn_st_stock_sel(df_all):
         fn_st_add_space(1)
         sels = st.columns([1, 1, 1, 0.1, 0.45, 0.45, 0.45])
 
-        sid_2_watch = sels[0].text_input('手動篩選:', value='2851, 4562, 3426', key='sid_2_watch')
+        sid_2_watch = sels[0].text_input('手動篩選:', value='2851, 4562, 3426, 2404', key='sid_2_watch')
         dic_cfg["sel_rat"] = sels[1].slider('勝率門檻(%)', min_value=40, max_value=100, value=50)
         dic_cfg["sel_corr"] = sels[2].slider('相關性門檻', min_value=0.5, max_value=1.0, value=0.9)
         # dic_cfg["sel_price"] = sels[2].slider('股價上限', min_value=0, max_value=500, value=500)
